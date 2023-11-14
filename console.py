b9bb0bcdede175ec2dee29986c90372745f9a910
Mthobeki <mthobekisikhosanag@gmail.com>
2023-11-14 14:50:29 +0200

    extra

--- console.py
+++ console.py
@@ -4,7 +4,6 @@ console.py module: Contains the entry point of the command interpreter.
 """
 
 import cmd
-import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -96,51 +95,34 @@ class HBNBCommand(cmd.Cmd):
         obj_dict = storage.all()
         if arg:
             if arg in self.class_dict:
-                print([str(obj) for obj in self.class_dict[arg].all()])
+                print([str(obj) for key, obj in obj_dict.items() if isinstance(obj, self.class_dict[arg])])
             else:
                 print("** class doesn't exist **")
         else:
             print([str(obj) for obj in obj_dict.values()])
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and ID with a dictionary."""
+        """Updates an instance based on the class name and ID by adding or updating an attribute."""
         args = arg.split()
-        if len(args) < 3:
-            print("** class name, instance id, and dictionary representation are required **")
+        if len(args) < 4:
+            print("** class name, instance id, attribute name, and value are required **")
             return
 
-        class_name, instance_id, json_dict = args[0], args[1], ' '.join(args[2:])
+        class_name, instance_id, attribute_name, value = args
         obj_dict = storage.all()
         key = f"{class_name}.{instance_id}"
 
         if key in obj_dict:
             obj = obj_dict[key]
             try:
-                # Use json.loads to parse the dictionary representation
-                update_dict = json.loads(json_dict.replace("'", "\""))
-                for attr, value in update_dict.items():
-                    # Use proper type conversion based on the attribute type
-                    setattr(obj, attr, type(getattr(obj, attr))(value))
+                # Use proper type conversion based on the attribute type
+                setattr(obj, attribute_name, type(getattr(obj, attribute_name))(value))
                 obj.save()
-            except ValueError:
-                print("** invalid dictionary representation **")
+            except AttributeError:
+                setattr(obj, attribute_name, value)
+                obj.save()
         else:
             print("** no instance found **")
-
-    def do_count(self, arg):
-        """Counts the number of instances of a class."""
-        args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-
-        class_name = args[0].strip()
-        if class_name not in self.class_dict:
-            print("** class doesn't exist **")
-            return
-
-        count = len([obj for obj in storage.all().values() if isinstance(obj, self.class_dict[class_name])])
-        print(count)
 
 
 if __name__ == '__main__':
